c7e1aba8b99e73d696518f408df387745994485a
Cecilj24 <clangba@yahoo.com>
2024-01-02 20:32:54 -0800

    started special offers section and input the images

--- src/components/ServiceCard.jsx
+++ src/components/ServiceCard.jsx
@@ -1,13 +1,20 @@
+/* eslint-disable react/prop-types */
 const ServiceCard = ({ imgURL, label, subtext }) => {
     return (
         <div className="flex-1 sm:w-[]350px sm:min-w-[350px] w-full
         rounded shadow-3xl px-10 py-16">
-            <div>
+            <div className="flex w-11 h-11 justify-center rounded-full items-center bg-coral-red">
                 <img src={imgURL}
-                    alt=""
+                    alt={label}
+                    width={24}
+                    height={24}
 
                 />
             </div>
+            <h3 className="mt-5 font-palanquin text-3xl leading-normal font-bold"
+            >{label}</h3>
+            <p className="mt-3 font-palanquin break-words text-lg leading-normal text-slate-gray">
+                {subtext}</p>
 
         </div>
     )
--- src/sections/SpecialOffer.jsx
+++ src/sections/SpecialOffer.jsx
@@ -1,6 +1,46 @@
+import { offer } from "../assets/images"
+import Button from "../components/Button"
+
 const SpecialOffer = () => {
     return (
-        <div>SpecialOffer</div>
+        <section className="flex justify-wrap items-center max-xl:flex-col-reverse gap-10 max-container">
+            <div>
+                <img src={offer}
+                    alt=""
+                    width={773}
+                    height={687}
+                    className="object-contain w-full"
+
+                />
+            </div>
+            <div className="flex flex-1 flex-col">
+                <h2 className="font-palanquin text-4xl capitalize
+                font-bold lg:max-w-lg">
+                    <br />
+                    <span className="
+                    text-coral-red inline-block
+                    mt-3"> Special</span>
+                    <br />
+                    Offer
+                </h2>
+                <p className="mt-4 lg:max-w-lg info-text">
+                    Dont miss out! Check out our special offers and embark on
+                    a shopping journey with unbeatable deals.
+                </p>
+                <p className="mt-6 lg:max-w-lg info-text">
+                    Our dedication to detail and excellence ensures your
+                    Satisfaction
+                </p>
+                <div className="mt-11">
+                    <Button
+                        label="View Details"
+
+                    />
+
+                </div>
+            </div>
+
+        </section>
     )
 }
 
